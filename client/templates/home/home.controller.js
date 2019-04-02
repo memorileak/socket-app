55d8c262f9d1fa65deb449bd57307221c05a9f6c
TungNguyen <tungnvan.mar12th@gmail.com>
2019-04-02 23:55:14 +0700

    Add send & receive message

--- client/templates/home/home.controller.js
+++ client/templates/home/home.controller.js
@@ -1,4 +1,43 @@
-SocketIOApp.controller('homeController', function() {
+SocketIOApp.controller('homeController', function($scope) {
     const sc = this;
-    sc.name = 'Tung';
+    const BASE_URL = 'http://' + window.location.host;
+    sc.user = '';
+    sc.client = '';
+    sc.message_text = '';
+    sc.clients = [];
+    sc.messages = [];
+    const socket = io();
+
+    socket.on('joined', (user) => {
+        sc.user = user;
+        console.log("Your id: ", sc.user);
+        sc.getListClients(user);
+    });
+
+    socket.on('receive_message', ({from, text}) => {
+        $scope.$apply(sc.messages.push({from, text}));
+    });
+
+    socket.on('someone_joined', () => {sc.getListClients(sc.user)});
+    socket.on('someone_left', () => {sc.getListClients(sc.user)});
+
+    sc.getListClients = function(user) {
+        $.get(`${BASE_URL}/clients?user=${user}`, (res) => {
+            $scope.$apply(sc.clients = res.data.clients);
+        });
+    };
+
+    sc.sendMessage = function() {
+        if (sc.client) {
+            socket.emit('send_message', {to: sc.client, text: sc.message_text});
+            sc.message_text = "";
+        } else {
+            alert('No user selected!');
+        }
+    };
+
+    sc.handleUserClick = function(client) {
+        sc.client = client;
+    };
+
 });
--- index.js
+++ index.js
@@ -2,25 +2,44 @@ const express = require('express');
 const app = express();
 const httpServer = require('http').Server(app);
 const io = require('socket.io')(httpServer);
+const cors = require('cors');
 const port = 1206;
 
 const clients = {};
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.static('client'));
+app.use(express.static('client/assets'));
 
 io.on('connection', (socket) => {
     console.log('a user connected: ' + socket.id);
 
+    socket.emit('joined', socket.id);
+    socket.broadcast.emit('someone_joined');
+
     socket.on('disconnect', () => {
         console.log('a user disconnected: ' + socket.id);
         delete clients[socket.id];
+        socket.broadcast.emit('someone_left');
     });
     
     clients[socket.id] = socket;
 
     socket.on('send_message', ({to, text}) => {
-        clients[to].emit('receive_message', {from: socket.id, text});
+        if (clients[to]) {
+            clients[to].emit('receive_message', {from: socket.id, text});
+            socket.emit('receive_message', {from: socket.id, text});
+        }
+    });
+});
+
+app.get('/clients', (req, res) => {
+    const user_id = req.query.user || null;
+    res.json({
+        success: true,
+        code: 200,
+        data: {clients: Object.keys(clients).filter(client => client !== user_id) || []}
     });
 });
 
